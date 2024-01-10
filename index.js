496d636ce762c742323a81e8e83e2f1d3b110b25
wachuka7 <rachaelwachuka7@gmail.com>
2024-01-11 00:17:26 +0300

    Destricturing lab

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const farmAnimals = 'cow horse sheep pig chicken';
+const [moo, neigh, baa, oink, cluck]= farmAnimals.split(' ');
+const [bessie, ,dolly, babe, little]=farmAnimals.split(' ')
 
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 
